ec3a78750290dac81fd1ada69647b0de0ebc2900
mayconbalves <mayconmustaine@hotmail.com>
2021-08-26 20:16:04 -0300

    feat(hex-colors-genarators): create and implements change colors function

--- hex-colors-generators/index.js
+++ hex-colors-generators/index.js
@@ -1,3 +1,18 @@
+const body = document.getElementsByTagName('body')[0]
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const letters = ['a', 'b', 'c', 'd', 'e', 'f']
+
+let a, b = ''
+
+
 function changeColor() {
-  alert(`The color is now`)
+  const test = []
+  for (let i = 0; i < 3; i++) {
+    a = letters[Math.floor(Math.random() * letters.length)]
+    b = numbers[Math.floor(Math.random() * numbers.length)]
+    test.push(a, b)
+  }
+
+  return body.style.backgroundColor = `#${test.join('')}`
 }
