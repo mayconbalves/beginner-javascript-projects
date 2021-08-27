692ca860a94f77f797f83398f22a4caaecd67a61
mayconbalves <mayconmustaine@hotmail.com>
2021-08-26 21:35:32 -0300

    refactor(hex-colors-genarators): change vars names

--- hex-colors-generators/index.js
+++ hex-colors-generators/index.js
@@ -1,18 +1,18 @@
 const body = document.getElementsByTagName('body')[0]
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 const letters = ['a', 'b', 'c', 'd', 'e', 'f']
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-let a, b = ''
+let concatLetters, concatNumbers = ''
 
 
 function changeColor() {
-  const test = []
+  const hexColor = []
   for (let i = 0; i < 3; i++) {
-    a = letters[Math.floor(Math.random() * letters.length)]
-    b = numbers[Math.floor(Math.random() * numbers.length)]
-    test.push(a, b)
+    concatLetters = letters[Math.floor(Math.random() * letters.length)]
+    concatNumbers = numbers[Math.floor(Math.random() * numbers.length)]
+    hexColor.push(concatLetters, concatNumbers)
   }
 
-  return body.style.backgroundColor = `#${test.join('')}`
+  return body.style.backgroundColor = `#${hexColor.join('')}`
 }
