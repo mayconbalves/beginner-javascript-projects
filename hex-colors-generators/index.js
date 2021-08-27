f613e4daab695dccc3b1d19327e95b84db4b22c2
mayconbalves <mayconmustaine@hotmail.com>
2021-08-26 22:12:36 -0300

    feat(hex-colors-genarators): implements dinamic hex color

--- hex-colors-generators/index.js
+++ hex-colors-generators/index.js
@@ -1,10 +1,11 @@
 const body = document.getElementsByTagName('body')[0]
+const button = document.getElementsByTagName('button')[0]
+const colorShow = document.getElementsByTagName('span')[0]
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f']
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 let concatLetters, concatNumbers = ''
-
 
 function changeColor() {
   const hexColor = []
@@ -13,6 +14,7 @@ function changeColor() {
     concatNumbers = numbers[Math.floor(Math.random() * numbers.length)]
     hexColor.push(concatLetters, concatNumbers)
   }
-
-  return body.style.backgroundColor = `#${hexColor.join('')}`
+  button.style.backgroundColor = `#${hexColor.join('')}`
+  body.style.backgroundColor = `#${hexColor.join('')}`
+  colorShow.innerHTML = `#${hexColor.join('')}`
 }
