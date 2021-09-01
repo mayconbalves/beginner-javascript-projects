3bf04d2b9b255d85d2e6bf17ba8d0a59f2123d08
mayconbalves <mayconmustaine@hotmail.com>
2021-08-31 21:24:06 -0300

    feat(random-quotes-generator): implements onload method

--- random-quotes-generator/script.js
+++ random-quotes-generator/script.js
@@ -1,6 +1,18 @@
+const divQuotes = document.getElementById('quotes')
 
 const fetchQuotes = async () => {
   const data = await fetch('https://type.fit/api/quotes')
   const quotes = await data.json()
   return quotes[Math.floor(Math.random() * quotes.length)]
 }
+
+const initialQuotes = async () => {
+  const data = await fetch('https://type.fit/api/quotes')
+  const quotes = await data.json()
+  const quotePhrase = document.createElement('p')
+  quotePhrase.innerHTML = quotes[Math.floor(Math.random() * quotes.length)].text
+
+  divQuotes.appendChild(quotePhrase)
+}
+
+window.onload = initialQuotes()
