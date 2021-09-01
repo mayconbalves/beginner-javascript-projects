de18b12d2de2b68aeda9e3fd670de3645dcd1663
mayconbalves <mayconmustaine@hotmail.com>
2021-08-31 21:31:27 -0300

    feat(random-quotes-generator): implements random quotes when button is clicked

--- random-quotes-generator/script.js
+++ random-quotes-generator/script.js
@@ -3,7 +3,10 @@ const divQuotes = document.getElementById('quotes')
 const fetchQuotes = async () => {
   const data = await fetch('https://type.fit/api/quotes')
   const quotes = await data.json()
-  return quotes[Math.floor(Math.random() * quotes.length)]
+  const quotePhrase = document.getElementsByTagName('p')[0]
+  quotePhrase.innerHTML = quotes[Math.floor(Math.random() * quotes.length)].text
+
+  return divQuotes.appendChild(quotePhrase)
 }
 
 const initialQuotes = async () => {
